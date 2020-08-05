a21a3a813be7745dd690f61664c144060508a0ba
JBrickhouse <jbrickhouse777@gmail.com>
2020-08-05 15:25:20 -0500

    Successfull data calls

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 
 
-
-
-function buildQueryURL () {
-
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?";
+// Building the queryURL to get current weather data
+function buildCurrentQueryURL () {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?";
 
     var queryParameters = {"appid": "f5e728105e3af40a0f53311e5edbb7c8"};
+    queryParameters.q = $("#cityInput").val().trim();
 
+    console.log(queryURL + $.param(queryParameters));
+    return(queryURL + $.param(queryParameters))
+}
+
+// Building the queryURL to get forecasted weather data
+function buildForecastQueryURL () {
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?";
+    
+    var queryParameters = {"appid": "f5e728105e3af40a0f53311e5edbb7c8"};
     queryParameters.q = $("#cityInput").val().trim();
 
     console.log(queryURL + $.param(queryParameters));
@@ -16,10 +24,9 @@ function buildQueryURL () {
 
 
 
-
+// Generating an array of the next five days after today
+// Since the hottest temperature is usually at noon, I've appended 12:00:00
 function generateFutureDates () {
-    // Generating an array of the next five days after today
-    // Since the hottest temperature is usually at noon, I've appended 12:00:00
     var todayDate = moment().format('YYYY-MM-DD');
     futureDays = new Array(5);
     for (i=0; i < futureDays.length; i++) {
@@ -30,31 +37,67 @@ function generateFutureDates () {
 }
 
 
-function getWeatherData() {
+function getCurrentWeather(weatherData) {
 
     // Getting today's date
     var todayDate = moment().format('YYYY-MM-DD');
+ 
+    var tempK = weatherData.main.temp;
+    tempF = ((tempK - 273.15) * 1.8) + 32;
+    tempC = (tempK - 273.15);
+
+    var humidPercent = weatherData.main.humidity;
+
+    var windSpeed = weatherData.wind.speed;
+
     
+
+    // Weather for Current Day and Time
+
+    // Store all the data in localStorage
+
+}
+
+function getForecastWeather(weatherData) {
+
     // Getting the next five dates after today
     var futureDays = generateFutureDates();
+
+    // console.log(futureDays);
+
+
+    // Weather for future days and times
     
-    console.log(todayDate);
-    console.log(futureDays);
-    
-    
+    // Store all the data in localStorage
+
 }
+
+    
 
 
 $("#searchButton").on("click", function(event) {
     
     event.preventDefault();
     
-    var queryURL = buildQueryURL();
+    var queryURLcurrentWeather = buildCurrentQueryURL();
+    var queryURLforecastWeather = buildForecastQueryURL();
 
     $.ajax({
-        url: queryURL,
+        url: queryURLcurrentWeather,
         method: "GET"
-    }).then(getWeatherData);
+    }).then(getCurrentWeather);
+
+    $.ajax({
+        url: queryURLforecastWeather,
+        method: "GET"
+    }).then(getForecastWeather);
+
+
+    // Add a button with the city name
+    // Append it to the search history list
+
+    // var cityName = $("#cityInput").val().trim();
+
 });
 
 
